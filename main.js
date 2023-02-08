18fec1ef6b57f8f3f4b4a2c134a9adc57dc899f5
LINGARAJ <lingaraj01052002@gmail.com>
2023-02-08 16:25:22 +0530

    adding js-hours1.

---
+++ main.js
@@ -0,0 +1,21 @@
+let time = new Date();
+hour=time.getHours();
+
+if(hour<3){
+	document.write("midnight");
+}
+else if(hour<12){
+	document.write("morning");
+}
+else if(hour<15){
+	document.write("afternoon");
+}
+else if(hour<18){
+	document.write("evening");
+}
+else if(hour<24){
+	document.write("night");
+}
+else{
+	document.write("")
+}
